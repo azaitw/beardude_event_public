e051f5eb0e24c380dacf7e23bfd06a8d6668027b
jimhcc <jimhcc@yhoo-inc.com>
2017-08-25 01:11:15 +0800

    useless comment

--- src/lib/db.js
+++ src/lib/db.js
@@ -6,8 +6,6 @@ export default async function loadDB() {
   try {
     firebase.initializeApp(fireBaseConfig)
   } catch (err) {
-    // we skip the "already exists" message which is
-    // not an actual error when we're hot-reloading
     if (!/already exists/.test(err.message)) {
       console.error('Firebase initialization error', err.stack)
     }
