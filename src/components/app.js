87a536981d6715595b24dbb6e16ed1a2f1d51c1f
Azai Chan <azai.tw@gmail.com>
2017-08-31 20:01:00 +0800

    Add nodemon and devtools

--- src/components/app.js
+++ src/components/app.js
@@ -1,29 +1,29 @@
 import { h, Component } from 'preact'
 import { Router } from 'preact-router'
-
-import Header from './header'
+import 'preact/devtools'
 import Home from './home'
+import Eventlist from './Eventlist'
+import PublicEvent from './PublicEvent'
 import Profile from './profile'
 
-export default class App extends Component {
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
 
-	render() {
-		return (
-			<div id="app">
-				<Header />
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-				</Router>
-			</div>
-		);
-	}
+export default class App extends Component {
+/** Gets fired when the route changes.
+ *  @param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+ *  @param {string} event.url	The newly routed URL
+ */
+  handleRoute = e => {
+    this.currentUrl = e.url;
+  };
+
+  render() {
+    return (
+      <div id="app">
+        <Router onChange={this.handleRoute}>
+          <Eventlist path="/" />
+          <PublicEvent path="/event/:uniqueName" />
+        </Router>
+      </div>
+    );
+  }
 }
