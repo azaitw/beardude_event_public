2871383361e34dfbe64545b741f5ce8a276a710c
Azai Chan <azai.tw@gmail.com>
2017-10-30 11:28:22 +0800

    Add Youtube video quality param; add (beta)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'preact-router/match'
 import css from './style.css'
 
 const Header = ({name, uniqueName, navs}) => <div className={css.mainHeader}>
-  {uniqueName === undefined ? <div className={css.heading}><h1 className={css.bdlogo}><span>Beardude</span><span>Event</span></h1></div>
+  {uniqueName === undefined ? <div className={css.heading}><h1 className={css.bdlogo}><span>Beardude</span><span>Event</span> (beta)</h1></div>
     : <div className={css.heading}>
       <h1 className={css.eventName}><Link activeClassName={css.active} className={css.nav} href={`/event/${uniqueName}`}>{name}</Link></h1>
       <ul className={css.navContainer}>{navs.map(nav => <li key={'nav-' + nav.key}><Link activeClassName={css.navActive} className={css.nav} href={`/event/${uniqueName}/${nav.key}`}>{nav.name}</Link></li>)}</ul>
